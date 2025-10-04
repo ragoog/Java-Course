a25938fe65886bd2d4df613196783732efc96ef8
ragoog <ragiasidahmed212005@gmail.com>
2025-10-04 08:44:48 +0300

    Assignment 1

---
+++ Main.java
@@ -0,0 +1,100 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+    //q.8 : Test.id is wrong because we are treating it like a static field
+
+        Account account = new Account();
+        account.setId(1122);
+        account.setBalance(20000);
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+        System.out.println(account.getDate());
+        System.out.println(account.getBalance());
+        System.out.println(account.getMonthlyInterestRate());
+    }
+}
+
+class Account {
+
+    //fields
+    private int id;
+    private double balance;
+    private static double annualInterestRate;
+    private Date date;
+
+    //default constructor
+    public Account ()
+    {
+        id = 0;
+        balance = 0.0;
+        date= new Date();
+    }
+    //constructor
+    public  Account (int id, double balance)
+    {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+
+
+    public static double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+    public void setAnnualInterestRate(double AnnualInterestRate) {
+        annualInterestRate = AnnualInterestRate;    //static field
+    }
+
+
+    public  Date getDate()
+    {
+        return date;
+    }
+
+    
+public double getMonthlyInterestRate ()
+{
+    return annualInterestRate/12 ;
+}
+public double getMonthlyInterest()
+{
+    return balance * (getMonthlyInterestRate() / 100);
+}
+public void withdraw(double amount)
+{
+    if (amount <= balance) {
+        balance -= amount;
+    }
+}
+public void deposit(double amount)
+    {
+        if (amount >0) {
+            balance += amount;
+        }
+    }
+
+}
+
+
+
