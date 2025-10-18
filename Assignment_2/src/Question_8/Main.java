2d30128ba1df97d0549cd1932ebf0f52ae821852
ragoog <ragiasidahmed212005@gmail.com>
2025-10-18 10:42:13 +0300

    q8

---
+++ Assignment_2/src/Question_8/Main.java
@@ -0,0 +1,10 @@
+package Question_8;
+
+public class Main {
+    /* Q: If a method in a subclass has the same signature as a method in its superclass with the same return type,
+    is the method overridden or overloaded?
+
+    A: overridden --> because it is between super and subclass, and return type and signature are the same
+     */
+
+}
