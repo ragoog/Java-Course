2e0cc247bc2cf4937e5046d9dde7bcd7d085e8af
ragoog <ragiasidahmed212005@gmail.com>
2025-10-18 10:45:25 +0300

    q9

---
+++ Assignment_2/src/Question_9/Main.java
@@ -0,0 +1,11 @@
+package Question_9;
+
+public class Main {
+
+    /*
+    Q: If a method in a subclass has the same signature as a method in its superclass with a different return type,
+    will this be a problem?
+
+    A: yes because this way you are overriding but you are changing the return type which is invalid (compile time error)
+     */
+}
