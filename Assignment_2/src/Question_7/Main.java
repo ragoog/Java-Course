0353bf9fdd43a5ee76972f648a769d4e1fbfcfb9
ragoog <ragiasidahmed212005@gmail.com>
2025-10-18 10:37:53 +0300

    added q7

---
+++ Assignment_2/src/Question_7/Main.java
@@ -0,0 +1,15 @@
+package Question_7;
+
+public class Main {
+
+    /*
+    in class Circle's constructor --> this.radius = radius;
+    in class B's constructor --> super(radius);
+                                 this.length = length;
+
+   in getArea() method --> return super.getArea() * length;
+
+
+    */
+
+}
